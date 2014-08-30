006e82468b2ad48ef2a3951422dc440c5299fb7b
Kavimal Wijewardana <kavi707@gmail.com>
2014-08-30 20:51:48 +0530

    Add logger to application

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var express = require('express');
+var logger = require('./utils/log');
 var services = require('./services.js');
 
 var app = express();
 app.use(express.bodyParser());
 
 app.post('/:modelName', function (req, res) {
+    logger.info('NodeGrid:app/ [POST/:modelName]');
     services.handlePost(req, res);
 });
 
 app.get('/:modelName', function (req, res) {
+    logger.info('NodeGrid:app/ [GET/:modelName]');
     services.handleGet(req, res);
 });
 
@@ -17,9 +20,10 @@ app.put('/:modelName/:id', function (req, res) {
 });
 
 app.post('/:firstEntity/:firstIdentifier/:relationType/:secondEntity/:secondIdentifier', function (req, res) {
+    logger.info('NodeGrid:app/ [POST/:firstEntity/:firstIdentifier/:relationType/:secondEntity/:secondIdentifier]');
     services.handleRelationalPost(req, res);
 });
 
 var server = app.listen(3000, function () {
-    console.log('Listening on port %d', server.address().port);
+    logger.info('NodeGrid:app/ NodeGrid app started. Listen on port: ' + server.address().port);
 });
