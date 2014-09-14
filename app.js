73d7c1ccb81f6ae4648971f4c939a7c22845b83f
PahanPerera <pahan.madushanka@gmail.com>
2014-09-14 20:30:17 +0530

    adding server side push

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var logger = require('./utils/log');
+var io = require('socket.io');
 
 var storingEndPoints = require('./end_points/store_end_points');
 var relationsEndPoints = require('./end_points/relations_end_points');
 var queryEndPoints = require('./end_points/query_end_points');
+var pushEndPoints = require('./end_points/push_end_points');
 
 var app = express();
 app.use(express.bodyParser());
@@ -12,7 +14,12 @@ storingEndPoints.createStoreEndPoints(app);
 relationsEndPoints.createRelationsEndPoints(app);
 queryEndPoints.createQueryEndPoints(app);
 
+
 //starting the server
 var server = app.listen(3000, function () {
     logger.info('NodeGrid:app/ NodeGrid app started. Listen on port: ' + server.address().port);
 });
+
+// setting server io
+var server_io = io.listen(server);
+pushEndPoints.createPushEndPoints(app, server_io);
