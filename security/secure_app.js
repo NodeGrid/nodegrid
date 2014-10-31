772d5aabe11380483e53e0df34dd7594e1ffe5d8
Kavimal Wijewardana <kavi707@gmail.com>
2014-10-31 11:15:30 +0530

    Check mongo connection status before all requests

--- security/secure_app.js
+++ security/secure_app.js
@@ -1,20 +1,31 @@
 var tokenMaster = require('../utils/token_master');
 var utils = require('../utils/utils');
+var logger = require('../utils/log');
+var mongo_connection = require('../utils/mongoose_connection');
 
 module.exports.setSecureApp = function (req, res, next) {
-	console.log(req.path);
-	if(req.path.lastIndexOf('/system/') == 0){
-		next();
-		return;
-	}
-	var accessToken = req.headers.authorization;
-    tokenMaster.validateAccessToken(accessToken, function (status, response) {
-        if (status == 1) {
+    logger.info(req.path);
+
+    //check mongo db connection availability
+    var mongo_connection_status = mongo_connection.mongoConnectionStatus();
+
+    if (mongo_connection_status == 'CONNECTED') {
+        if (req.path.lastIndexOf('/system/') == 0) {
             next();
-        } else if (status == 2){
-            utils.sendResponse(res, 404, 'Not found - No valid accessToken', 'EMPTY');
-        } else if (status == 3) {
-            utils.sendResponse(res, 401, 'Unauthorized - Token expired', response);
+            return;
         }
-    });
+        var accessToken = req.headers.authorization;
+        tokenMaster.validateAccessToken(accessToken, function (status, response) {
+            if (status == 1) {
+                next();
+            } else if (status == 2) {
+                utils.sendResponse(res, 404, 'Not found - No valid accessToken', 'EMPTY');
+            } else if (status == 3) {
+                utils.sendResponse(res, 401, 'Unauthorized - Token expired', response);
+            }
+        });
+    } else {
+        //mongo db connection is not available
+        utils.sendResponse(res, 503, 'Service Unavailable - MongoDB connection unavailable', 'EMPTY');
+    }
 };
