b5efe48f98441e961bdf5acb2a090ae09bc8994b
Kavimal Wijewardana <kavi707@gmail.com>
2014-11-27 17:56:06 +0530

    Process the query parameters for advance quering

--- end_points/query_end_points.js
+++ end_points/query_end_points.js
@@ -4,6 +4,7 @@
  */
 var logger = require('../utils/log');
 var queryServices = require('../services/query_services');
+var url = require('url');
 var dbService = require('../db_callings/query_db_callings');
 
 /**
@@ -11,6 +12,36 @@ var dbService = require('../db_callings/query_db_callings');
  * @param app
  */
 module.exports.createQueryEndPoints = function (app) {
+
+    app.get('/app/advance/:modelName', function (req, res) {
+        var url_parts = url.parse(req.url, true);
+        var query = url_parts.query;
+
+        console.log(query);
+
+        for (var key in query) {
+            if (key === 'qry')
+                var qry = query.qry;
+            else if (key === 'sort')
+                var sort = query.sort;
+            else if (key === 'limit')
+                var limit = query.limit;
+        }
+
+        if (qry != null) {
+            var mainObj = qry.split('where');
+            var select = mainObj[0].split('select')[1].trim();
+
+            console.log(select);
+
+            var where = mainObj[1].split(' and ');
+
+            console.log(where);
+        }
+
+        res.send({});
+        //dbService.getOneFromDBHardCoded(req, res);
+    });
 
     // Retrieve all data from given model (collection)
     app.get('/app/:modelName', function (req, res) {
@@ -20,11 +51,7 @@ module.exports.createQueryEndPoints = function (app) {
 
     // Retrieve specific data from given model (collection)
     app.get('/app/:modelName/:id', function (req, res) {
-        logger.info('NodeGrid:query_end_points/createQueryEndPoints - [GET/app/:modelName]');
+        logger.info('NodeGrid:query_end_points/createQueryEndPoints - [GET/app/:modelName/:id]');
         queryServices.handleQueryModelGetOne(req, res);
     });
-
-    app.get('/hardcode', function (req, res) {
-         dbService.getOneFromDBHardCoded(req, res);
-    })
 };
