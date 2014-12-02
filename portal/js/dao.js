f2b934e1e128f6bf806b2b46c905dc2aecbe4258
PahanPerera <pahan.madushanka@gmail.com>
2014-12-02 14:01:19 +0530

    adding ng-view to portal

--- portal/js/dao.js
+++ portal/js/dao.js
@@ -1,7 +1,31 @@
 var API_URL = "http://localhost:3000";
 
 
-var app = angular.module('app', ['ngCookies']);
+var app = angular.module('app', ['ngCookies','ngRoute']);
+
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/login', {
+        templateUrl: 'web/login.html',
+        controller: 'loginController'
+    }).
+        when('/login/:status', {
+        templateUrl: 'web/login.html',
+        controller: 'loginController'
+        }).
+      when('/app', {
+        templateUrl: 'web/app.html',
+        controller: 'collectionsController'
+      }).
+        when('/register', {
+        templateUrl: 'web/register.html',
+        controller: 'registerController'
+      }).
+      otherwise({
+        redirectTo: '/login'
+      });
+}]);
 
 app.controller("collectionsController", function collectionsController($scope, $http, $window, $cookieStore) {
 
@@ -14,7 +38,7 @@ app.controller("collectionsController", function collectionsController($scope, $
     })
     .error(function(error) {
         console.log(error);
-          $window.location.href = 'login.html';
+          $window.location.href = '#login';
     });
     $scope.onCollectionClick = function(item){
         $scope.collectionActive = {};
@@ -38,7 +62,7 @@ app.controller("collectionsController", function collectionsController($scope, $
             $scope.collectionActive[item] = "active";
         })
         .error(function(error) {
-          $window.location.href = 'login.html';
+          $window.location.href = '#login';
         });
     };
 
@@ -50,4 +74,44 @@ app.controller("collectionsController", function collectionsController($scope, $
         $scope.itemActive[row._id] = "active";
     };
 
+});
+
+app.controller("loginController", function($scope, $http, $window, $cookieStore,$routeParams){
+
+    
+    if($routeParams.status){
+       $scope.status = $routeParams.status    
+    }
+    
+    $scope.login = function(username,password){
+
+            $http({
+                url: API_URL+"/system/security/generateToken",
+                method: "POST",
+                data: { 'username' : username, "password":password}
+                }).success(function(responce){
+                    $cookieStore.put('token',responce.data.data.accessToken);
+                    $window.location.href ='#app';
+                }).error(function(err){
+                    console.log(err);
+                    $scope.loginErr = "Invalid Username and Password .. !!";
+                });
+    };
+});
+
+app.controller("registerController", function($scope, $http, $window, $cookieStore){
+
+    $scope.register = function(username,name, password){
+
+            $http({
+                url: API_URL+"/system/user",
+                method: "POST",
+                data: { 'username' : username, "name":name, "password":password}
+                }).success(function(responce){
+                    $window.location.href ='#login/success';
+                }).error(function(err){
+                    console.log(err);
+                    $scope.loginErr = "Error While Registering the User";
+                });
+    };
 });
