5d5e1d47e2a9ddcce80931b72fce504751b56c04
Kavimal Wijewardana <kavi707@gmail.com>
2015-05-09 13:39:12 +0530

    Changed the response object only data or res

--- core/utils/utils.js
+++ core/utils/utils.js
@@ -42,15 +42,13 @@ module.exports.sendResponse = function (res, statusCode, statusMessage, dataObje
             responseObj = {
                 "status":"ERROR",
                 "msg":statusMessage,
-                "res":"N/A",
                 "data":dataObject
             };
         } else {
             responseObj = {
                 "status":"ERROR",
                 "msg":statusMessage,
-                "res":dataObject,
-                "data":"N/A"
+                "res":dataObject
             };
         }
     } else {
